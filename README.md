e2259d68df41a508a6a0932016133010df205785
GisoreBrian <freelancerkazungudev@gmail.com>
2024-05-26 09:27:09 +0300

    Trendy anime app commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# trendy_anime_app
+
+A new Flutter project to shows trendy anime content related to Anime from Anilist.
+
+## Screenshots
+
---
+++ android/app/src/main/kotlin/com/kazungudev/trendy_anime_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kazungudev.trendy_anime_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
